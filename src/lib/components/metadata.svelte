9d24fad98edb4c01b9f941e03727b258c6060837
Benjamin Blanke <be.blanke@pm.me>
2026-09-04 12:06:44 +0200

    Remove image meta tags from metadata.svelte
    Removed Open Graph and Twitter image meta tags.

--- src/lib/components/metadata.svelte
+++ src/lib/components/metadata.svelte
@@ -27,7 +27,6 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={imageUrl} />
 
 	<!-- Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image" />
@@ -35,5 +34,4 @@
 	<meta property="twitter:url" content={baseUrl} />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={imageUrl} />
 </svelte:head>
