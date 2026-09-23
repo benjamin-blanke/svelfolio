cdc11bc326ed785470498815d4806e317b409fa4
Benjamin Blanke <be.blanke@pm.me>
2026-09-23 14:22:48 +0200

    chore: remove legacy analytics endpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { MediaQuery } from 'svelte/reactivity';
-	import { PUBLIC_UMAMI_TRACKING_ID } from '$env/static/public';
 
 	import '../app.css';
-	import { dev } from '$app/environment';
 	import Header from '$lib/components/layout/header/header.svelte';
 	import Navbar from '$lib/components/layout/navbar/navbar.svelte';
 
@@ -58,9 +56,6 @@
 <svelte:head>
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	{@html data.siteSchemaHtml}
-	{#if !dev}
-		<script defer src="https://umami.wiscaksono.com/script.js" data-website-id={PUBLIC_UMAMI_TRACKING_ID}></script>
-	{/if}
 </svelte:head>
 
 <main
